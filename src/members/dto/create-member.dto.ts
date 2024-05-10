9366d06af755a2095df96de5d56cb3c485660e9e
Alex Oviedo <aoviedo@andean.tech>
2024-05-10 16:43:25 -0500

    DTO agregado

--- src/members/dto/create-member.dto.ts
+++ src/members/dto/create-member.dto.ts
@@ -1,1 +1,20 @@
-export class CreateMemberDto {}
+import { IsBoolean, IsIn, IsNumberString, IsString, Length, MaxLength, MinLength } from "class-validator";
+
+export class CreateMemberDto {
+  @IsNumberString()
+  @Length(8)
+  dni: string;
+
+  @IsString()
+  name: string;
+
+  @IsString()
+  lastname: string;
+
+  @IsString()
+  @IsIn(['Titular','Alterno'])
+  type: string;
+
+  @IsBoolean()
+  active: boolean;
+}
