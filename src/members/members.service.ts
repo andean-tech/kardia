3d8b072b0cc1c668b5bf35be5e5a6c6a0b90e0ca
Alex Oviedo <aoviedo@andean.tech>
2024-05-20 16:40:52 -0500

    Avance

--- src/members/members.service.ts
+++ src/members/members.service.ts
@@ -1,11 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateMemberDto } from './dto/create-member.dto';
 import { UpdateMemberDto } from './dto/update-member.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Member } from './entities/member.entity';
 
 @Injectable()
 export class MembersService {
-  create(createMemberDto: CreateMemberDto) {
-    return 'This action adds a new member';
+
+  private readonly logger = new Logger('MemberService');
+  
+  constructor(
+    @InjectRepository(Member)
+    private readonly memberRepository: Repository<Member>
+  ){}
+
+  async create(createMemberDto: CreateMemberDto) {
+    try{
+      const member = this.memberRepository.create(createMemberDto);
+      await this.memberRepository.save(member);
+      return member;
+    }catch(error){
+      console.log(error); 
+      if(error.sqlState === '23000')
+        throw new BadRequestException(error.sqlMessage);
+
+      this.logger.error(error);
+      throw new InternalServerErrorException('Error desconocido en la aplicación');
+    }
   }
 
   findAll() {
